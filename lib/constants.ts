5bcb3144faf73363dfc8ebbf448c1b478626b40a
Dylan Wilson <dwilson5817@gmail.com>
2024-01-03 21:54:46 +0000

    fix(cdk): move certificate to eu-west-1
    The certificate must exist in the same region as the API Gateway.

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,5 @@
 export class Constants {
     static readonly frontendDomainName: string = 'free-market-fandango.dylanw.dev'
     static readonly apiDomainName: string = 'api.free-market-fandango.dylanw.dev'
-    static readonly certificateArn: string = 'arn:aws:acm:us-east-1:094951992242:certificate/7aa4ea1a-b71f-4f26-b1ef-dd38c6622d9f'
+    static readonly certificateArn: string = 'arn:aws:acm:eu-west-1:094951992242:certificate/57500dfa-14a2-438a-be04-ec54571aeb49'
 }
