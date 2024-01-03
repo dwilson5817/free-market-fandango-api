bcb5a2d02853abb06b7557f94762183fea12ede7
Dylan Wilson <dwilson5817@gmail.com>
2024-01-03 21:40:37 +0000

    feat(cdk): deploy Python code to Lambda
    Deploy the previously built Python code as a Lambda function.

---
+++ lib/constants.ts
@@ -0,0 +1,5 @@
+export class Constants {
+    static readonly frontendDomainName: string = 'free-market-fandango.dylanw.dev'
+    static readonly apiDomainName: string = 'api.free-market-fandango.dylanw.dev'
+    static readonly certificateArn: string = 'arn:aws:acm:eu-west-1:805294971713:certificate/a900dc63-8c28-4e20-b462-000928956874'
+}
--- lib/free-market-fandango-api-stack.ts
+++ lib/free-market-fandango-api-stack.ts
@@ -1,16 +1,46 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as certificatemanager from "aws-cdk-lib/aws-certificatemanager";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import { ApiGatewayToLambda } from "@aws-solutions-constructs/aws-apigateway-lambda";
+import { Constants } from "./constants";
+import * as path from "node:path";
 
 export class FreeMarketFandangoApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const certificate = certificatemanager.Certificate.fromCertificateArn(this, 'Certificate', Constants.certificateArn);
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'FreeMarketFandangoApiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new ApiGatewayToLambda(this, 'API', {
+      lambdaFunctionProps: {
+        runtime: lambda.Runtime.PYTHON_3_10,
+        handler: 'free-market-fandango-api.main.handler',
+        code: lambda.Code.fromAsset( path.join(__dirname, '../lambda.zip') ),
+        timeout: cdk.Duration.seconds(15),
+        environment: {
+          ADMIN_PASSWORD: process.env.ADMIN_PASSWORD || '',
+          SECRET_KEY: process.env.SECRET_KEY || '',
+          SPOTIPY_CLIENT_ID: process.env.SPOTIPY_CLIENT_ID || '',
+          SPOTIPY_CLIENT_SECRET: process.env.SPOTIPY_CLIENT_SECRET || '',
+          SPOTIPY_REDIRECT_URI: process.env.SPOTIPY_REDIRECT_URI || ''
+        },
+      },
+      apiGatewayProps: {
+        restApiName: this.stackName,
+        defaultCorsPreflightOptions: {
+          allowOrigins: [ `https://${Constants.frontendDomainName}` ],
+          allowMethods: [ 'GET', 'PUT', 'POST', 'DELETE' ]
+        },
+        domainName: {
+          domainName: Constants.apiDomainName,
+          certificate: certificate,
+        },
+        defaultMethodOptions: {
+          authorizationType: apigateway.AuthorizationType.NONE
+        }
+      }
+    });
   }
 }
