c6aec0429a7607d00c84ef808f4c1f99ba83ff41
Dylan Wilson <dwilson5817@gmail.com>
2024-01-15 20:04:53 +0000

    ci(cdk-deploy): use Docker to get Python dependencies
    Remove the `build-python` step and instead use the CDK construct to have
    this done automatically as part of `cdk deploy`.

--- lib/free-market-fandango-api-stack.ts
+++ lib/free-market-fandango-api-stack.ts
@@ -3,6 +3,7 @@ import { Construct } from 'constructs';
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as certificatemanager from "aws-cdk-lib/aws-certificatemanager";
 import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 import { ApiGatewayToLambda } from "@aws-solutions-constructs/aws-apigateway-lambda";
 import { Constants } from "./constants";
 import * as path from "node:path";
@@ -11,20 +12,33 @@ export class FreeMarketFandangoApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const table = new dynamodb.TableV2(this, 'FreeMarketFandango', {
+      partitionKey: { name: 'pk', type: dynamodb.AttributeType.STRING },
+    });
+
     const certificate = certificatemanager.Certificate.fromCertificateArn(this, 'Certificate', Constants.certificateArn);
 
     new ApiGatewayToLambda(this, 'API', {
       lambdaFunctionProps: {
         runtime: lambda.Runtime.PYTHON_3_10,
         handler: 'free-market-fandango-api.main.handler',
-        code: lambda.Code.fromAsset( path.join(__dirname, '../free-market-fandango-api') ),
+        code: lambda.Code.fromAsset( path.join(__dirname, '../free-market-fandango-api'), {
+          bundling: {
+            image: lambda.Runtime.PYTHON_3_10.bundlingImage,
+            command: [
+              'bash', '-c',
+              'pip install -r requirements.txt -t /asset-output && cp -au . /asset-output'
+            ],
+          },
+        }),
         timeout: cdk.Duration.seconds(15),
         environment: {
           ADMIN_PASSWORD: process.env.ADMIN_PASSWORD || '',
           SECRET_KEY: process.env.SECRET_KEY || '',
           SPOTIPY_CLIENT_ID: process.env.SPOTIPY_CLIENT_ID || '',
           SPOTIPY_CLIENT_SECRET: process.env.SPOTIPY_CLIENT_SECRET || '',
-          SPOTIPY_REDIRECT_URI: process.env.SPOTIPY_REDIRECT_URI || ''
+          SPOTIPY_REDIRECT_URI: process.env.SPOTIPY_REDIRECT_URI || '',
+          TABLE_NAME: table.tableName,
         },
       },
       apiGatewayProps: {
