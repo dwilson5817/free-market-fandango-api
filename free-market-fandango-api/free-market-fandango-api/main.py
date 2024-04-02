0816c13d5761e998645db86f1d422b4b58df14e1
Dylan Wilson <dwilson5817@gmail.com>
2024-04-02 21:37:55 +0000

    docs(fastapi): add API documentation
    Add some basic information such a license and contact information to the
    API.  This will be shown at the docs URL (`/docs`).

--- free-market-fandango-api/free-market-fandango-api/main.py
+++ free-market-fandango-api/free-market-fandango-api/main.py
@@ -3,7 +3,19 @@ from mangum import Mangum
 
 from .routers import auth
 
-app = FastAPI()
+app = FastAPI(
+    title="Free Market Fandango",
+    summary="A stock market themed party where purchases affect drink prices. 🍹",
+    contact={
+        "name": "Dylan Wilson",
+        "url": "https://dylanwilson.dev",
+        "email": "mail@dylanwilson.dev",
+    },
+    license_info={
+        "name": "GNU General Public License v3.0",
+        "url": "https://gitlab.dylanw.dev/free-market-fandango/api/-/raw/main/LICENSE",
+    },
+)
 
 app.include_router(auth.router)
 
