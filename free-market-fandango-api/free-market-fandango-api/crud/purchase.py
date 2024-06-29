032273507d9240de3a7767c1e6bf92480c516458
Dylan Wilson <dwilson5817@gmail.com>
2024-06-29 17:55:38 +0000

    feat(purchase): add purchase routers and CRUD operations
    Add router and CRUD file to work with purchases stored by the API.

---
+++ free-market-fandango-api/free-market-fandango-api/crud/purchase.py
@@ -0,0 +1,69 @@
+import datetime
+import decimal
+
+from boto3.dynamodb.conditions import Key
+
+from ..utils import build_key, explode_key, notify_purchase_event
+from ..constants import KeyComponents
+from ..schemas import PurchaseIn, PurchaseOut
+
+
+def _item_to_model(item):
+    _, card_number, timestamp = explode_key(item["SK"])
+
+    return PurchaseOut(
+        price=item["Price"],
+        stock_code=item["StockCode"],
+        card_number=card_number,
+        previous_balance=item["PreviousBalance"],
+        timestamp=datetime.datetime.fromisoformat(timestamp),
+    )
+
+
+def read_purchases(table, market_uuid: str):
+    response = table.query(
+        KeyConditionExpression=(
+                Key("PK").eq(build_key(KeyComponents.MARKET, market_uuid)) &
+                Key("SK").begins_with(build_key(KeyComponents.PURCHASE))
+        )
+    )
+
+    return [_item_to_model(item) for item in response["Items"]]
+
+
+def read_purchases_by_card_number(table, market_uuid: str, card_number: int):
+    response = table.query(
+        KeyConditionExpression=(
+                Key("PK").eq(build_key(KeyComponents.MARKET, market_uuid)) &
+                Key("SK").begins_with(build_key(KeyComponents.PURCHASE, card_number))
+        )
+    )
+
+    return [_item_to_model(item) for item in response["Items"]]
+
+
+def create_purchase(table, market_uuid: str, purchase_request: PurchaseIn, new_balance: decimal.Decimal) -> PurchaseOut:
+    purchase = PurchaseOut(**purchase_request.model_dump())
+
+    with table.batch_writer() as batch:
+        batch.put_item(
+            Item={
+                'PK': build_key(KeyComponents.MARKET, market_uuid),
+                'SK': build_key(KeyComponents.CARD, purchase.card_number),
+                'Balance': new_balance,
+            }
+        )
+
+        table.put_item(
+            Item={
+                "PK": build_key(KeyComponents.MARKET, market_uuid),
+                "SK": build_key(KeyComponents.PURCHASE, purchase.card_number, purchase.timestamp.isoformat()),
+                "StockCode": purchase.stock_code,
+                "PreviousBalance": purchase.previous_balance,
+                "Price": purchase.price,
+            }
+        )
+
+    notify_purchase_event(market_uuid, purchase.stock_code)
+
+    return purchase
---
+++ free-market-fandango-api/free-market-fandango-api/routers/purchase.py
@@ -0,0 +1,72 @@
+from fastapi import APIRouter, Depends, HTTPException
+
+from ..crud import market, purchase
+from ..dependencies import get_table, validate_jwt
+from ..schemas import PurchaseIn, PurchaseOut
+
+router = APIRouter(
+    prefix="/market/{market_uuid}/purchase",
+    tags=["purchases"],
+    dependencies=[Depends(get_table)],
+    responses={
+        400: {"description": "Bad Request"},
+        404: {"description": "Not Found"},
+    },
+)
+
+
+@router.get(
+    "/{card_number}",
+    response_model=list[PurchaseOut]
+)
+def read_purchases_for_card(market_uuid: str, card_number: int, table=Depends(get_table)):
+    return purchase.read_purchases_by_card_number(table, market_uuid, card_number)
+
+
+@router.post(
+    "",
+    response_model=PurchaseOut,
+    dependencies=[Depends(validate_jwt)],
+    responses={
+        400: {"description": "Bad Request"},
+        404: {"description": "Not Found"},
+    },
+)
+def create_purchase(market_uuid: str, new_purchase: PurchaseIn, table=Depends(get_table)):
+    current_market = market.read_market(table, market_uuid)
+
+    if not current_market:
+        raise HTTPException(status_code=404, detail="Market does not exist")
+
+    if not current_market.active:
+        raise HTTPException(status_code=400, detail="Market is closed")
+
+    stock_price = market.read_market_price(table, market_uuid, new_purchase.stock_code)
+
+    if not stock_price:
+        raise HTTPException(status_code=404, detail="Stock does not exist")
+
+    if stock_price.price != new_purchase.price:
+        raise HTTPException(status_code=400, detail="Stock price has changed, please refresh and submit again")
+
+    card_balance = market.read_market_balance(table, market_uuid, new_purchase.card_number)
+
+    if card_balance is None:
+        raise HTTPException(status_code=404, detail="Card does not exist")
+
+    new_purchase.previous_balance = card_balance.balance
+    new_balance = card_balance.balance - new_purchase.price
+
+    if new_balance < 0:
+        raise HTTPException(status_code=400, detail="Insufficient card balance")
+
+    return purchase.create_purchase(table, market_uuid, new_purchase, new_balance)
+
+
+@router.get(
+    "",
+    response_model=list[PurchaseOut],
+    dependencies=[Depends(validate_jwt)]
+)
+def read_purchases(market_uuid: str, table=Depends(get_table)):
+    return purchase.read_purchases(table, market_uuid)
