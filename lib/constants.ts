42535f6071c898a8c7408474bf10d945e27584f1
Dylan Wilson <dwilson5817@gmail.com>
2024-06-29 17:36:48 +0000

    feat(cdk): split API components into separate CDK constructs
    There are 3 components to the API:
    
    - the API responds to requests from the frontend
    - the queue handler actions events such as purchases and prices changes
    - the cron checks regularly for price changes and event rotations (and
      sends and message to an SQS queue to invoke the queue handler)
    
    For each of these, a new construct has been created to manage
    interactions in the CDK code between them.

--- lib/constants.ts
+++
@@ -1,5 +0,0 @@
-export class Constants {
-    static readonly frontendDomainName: string = 'market.dylanw.dev'
-    static readonly apiDomainName: string = 'market-api.dylanw.dev'
-    static readonly certificateArn: string = 'arn:aws:acm:eu-west-1:094951992242:certificate/733213f3-0e11-4e9a-ae5c-81f677bb5504'
-}
--- lib/free-market-fandango-api-stack.ts
+++ lib/free-market-fandango-api-stack.ts
@@ -1,66 +1,32 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as lambda from "aws-cdk-lib/aws-lambda";
-import * as certificatemanager from "aws-cdk-lib/aws-certificatemanager";
-import * as apigateway from "aws-cdk-lib/aws-apigateway";
 import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
-import { ApiGatewayToLambda } from "@aws-solutions-constructs/aws-apigateway-lambda";
-import { Constants } from "./constants";
-import * as path from "node:path";
+import { CronConstruct } from "./constructs/cron-construct";
+import { ApiConstruct } from "./constructs/api-construct";
+import { EventQueueConstruct } from "./constructs/event-queue-construct";
 
 export class FreeMarketFandangoApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const table = new dynamodb.TableV2(this, 'FreeMarketFandango', {
+    const dataTable = new dynamodb.TableV2(this, 'Data', {
       partitionKey: { name: 'PK', type: dynamodb.AttributeType.STRING },
       sortKey: { name: 'SK', type: dynamodb.AttributeType.STRING },
       removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
 
-    const certificate = certificatemanager.Certificate.fromCertificateArn(this, 'Certificate', Constants.certificateArn);
+    const eventQueue = new EventQueueConstruct(this, 'EventQueue', {
+      dataTable,
+    })
 
-    const apiGatewayToLambda = new ApiGatewayToLambda(this, 'API', {
-      lambdaFunctionProps: {
-        runtime: lambda.Runtime.PYTHON_3_10,
-        handler: 'free-market-fandango-api.main.handler',
-        code: lambda.Code.fromAsset( path.join(__dirname, '../free-market-fandango-api'), {
-          bundling: {
-            image: lambda.Runtime.PYTHON_3_10.bundlingImage,
-            command: [
-              'bash', '-c', 'pip install -r requirements.txt -t /asset-output && cp -au . /asset-output'
-            ],
-          },
-        }),
-        timeout: cdk.Duration.seconds(15),
-        environment: {
-          ADMIN_PASSWORD: process.env.ADMIN_PASSWORD || '',
-          SECRET_KEY: require('crypto').randomBytes(64).toString('hex'),
-          SPOTIPY_CLIENT_ID: process.env.SPOTIPY_CLIENT_ID || '',
-          SPOTIPY_CLIENT_SECRET: process.env.SPOTIPY_CLIENT_SECRET || '',
-          SPOTIPY_REDIRECT_URI: process.env.SPOTIPY_REDIRECT_URI || '',
-          TABLE_NAME: table.tableName,
-        },
-      },
-      apiGatewayProps: {
-        restApiName: this.stackName,
-        defaultCorsPreflightOptions: {
-          allowOrigins: [
-              `https://${Constants.frontendDomainName}`,  // Production
-              'http://localhost:5173'                     // Development
-          ],
-          allowMethods: [ 'GET', 'PUT', 'POST', 'DELETE' ]
-        },
-        domainName: {
-          domainName: Constants.apiDomainName,
-          certificate: certificate,
-        },
-        defaultMethodOptions: {
-          authorizationType: apigateway.AuthorizationType.NONE
-        }
-      }
+    new ApiConstruct(this, 'API', {
+      dataTable,
+      eventQueue: eventQueue.sqsQueue
+    })
+
+    new CronConstruct(this, 'Cron', {
+      dataTable,
+      eventQueue: eventQueue.sqsQueue
     });
-
-    table.grantReadWriteData(apiGatewayToLambda.lambdaFunction);
   }
 }
